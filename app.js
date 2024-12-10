70ac4c083510773f5f5884531269eb0d750d475c
Srivaishnavi08 <153370530+Srivaishnavi08@users.noreply.github.com>
2024-12-10 13:28:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const express=require('express');
+const app=express();
+const port=3000;
 
+app.get('/',(req,res)=>{
+  res.send('Hello, Docker');
+}
+        
+app.listen(3000,()=>{
+  console.log(`Server listenning at ${port}`);
+}
